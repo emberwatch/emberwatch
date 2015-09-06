804ab6799313ed8e85cea9e72f1a8fd39cd0fdc1
Philip Poots <philip.poots@gmail.com>
2015-09-06 23:21:29 +0200

    Talks up to August 15

--- _plugins/group_by_filter.rb
+++ _plugins/group_by_filter.rb
@@ -5,8 +5,10 @@ module Jekyll
         input.group_by do |item|
           time(item_property(item, property)).strftime("%B %Y")
         end.inject([]) do |memo, i|
-          memo << {"name" => i.first, "items" => i.last}
-        end
+          memo << {"name" => i.first, "items" => i.last.sort_by! {|hash| hash['date']}.reverse!, "sortable_date" => time(item_property(i.last.first, property)).strftime('%Y%m')}
+        end.sort_by! do |hash|
+          hash["sortable_date"]
+        end.reverse!
       else
         input
       end
